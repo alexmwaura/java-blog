10aa3a35c68ec6bd76d27a5c863b550d513b4c40
alexmwaura <alexmwaura43@gmail.com>
2020-10-29 19:02:09 +0300

    add new test case to get instance

--- src/main/java/models/Post.java
+++ src/main/java/models/Post.java
@@ -1,7 +1,11 @@
 package models;
 
 public class Post {
+    private String content;
     public Post(String content){
-
+        this.content = content;
+    }
+    public String getContent(){
+        return content;
     }
 }
