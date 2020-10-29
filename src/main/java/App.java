986e8e18c37e0539c138bd08bfedfe5186955cd3
alexmwaura <alexmwaura43@gmail.com>
2020-10-29 18:35:50 +0300

    add main static method

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,4 +1,11 @@
-package PACKAGE_NAME;
+import java.util.HashMap;
+import spark.ModelAndView;
+import spark.template.handlebars.HandlebarsTemplateEngine;
+import static spark.Spark.*;
 
 public class App {
+    public static void main(String[] args) {
+        staticFileLocation("/public");
+
+    }
 }
