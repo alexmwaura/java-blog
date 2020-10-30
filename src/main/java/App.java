80b7c123b6479bbfe813f186c921938bea0af753
alexmwaura <alexmwaura43@gmail.com>
2020-10-30 19:50:04 +0300

    add frontend logic

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,4 +1,8 @@
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Map;
+
+import models.Post;
 import spark.ModelAndView;
 import spark.template.handlebars.HandlebarsTemplateEngine;
 import static spark.Spark.*;
@@ -6,6 +10,17 @@ import static spark.Spark.*;
 public class App {
     public static void main(String[] args) {
         staticFileLocation("/public");
-
+        get("/", (req,res) -> {
+            Map<String, Object> model = new HashMap<String, Object>();
+            ArrayList<Post> posts = Post.getAll();
+            model.put("posts", posts);
+            return new ModelAndView(model, "index.hbs");
+        }, new HandlebarsTemplateEngine());
+        post("/posts/new", (req,res) -> {
+            Map<String, Object> model = new HashMap<String, Object>();
+            String content = req.queryParams("content");
+            Post newPost = new Post(content);
+            return new ModelAndView(model, "success.hbs");
+        }, new HandlebarsTemplateEngine());
     }
 }
