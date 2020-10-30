58350e019e4e76ed5ec39b5f48ebc815135150e3
alexmwaura <alexmwaura43@gmail.com>
2020-10-30 19:52:04 +0300

    add methods

--- src/main/java/models/Post.java
+++ src/main/java/models/Post.java
@@ -1,11 +1,21 @@
 package models;
 
+import java.util.ArrayList;
+
 public class Post {
+    private static ArrayList<Post> instances = new ArrayList<>();
     private String content;
     public Post(String content){
         this.content = content;
+        instances.add(this);
     }
     public String getContent(){
         return content;
+    }
+    public static ArrayList<Post> getAll(){
+        return instances;
+    }
+    public static void clearAllPost(){
+        instances.clear();
     }
 }
