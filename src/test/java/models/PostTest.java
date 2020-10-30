4594b302508129cec6406dd01734fb76564abd18
alexmwaura <alexmwaura43@gmail.com>
2020-10-30 19:51:17 +0300

    add test cases

--- src/test/java/models/PostTest.java
+++ src/test/java/models/PostTest.java
@@ -1,13 +1,15 @@
 package models;
 
+import javafx.geometry.Pos;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.util.ArrayList;
+
 import static org.junit.Assert.*;
 
 public class PostTest {
-
     @Test
     public void NewPostObjectGetsCorrectlyCreated_true() throws Exception {
         Post post = new Post("Day 1: Intro");
@@ -18,5 +20,22 @@ public class PostTest {
     public void PostInstantiatesWithContent_true() throws Exception{
         Post post = new Post("Day 1: Intro");
         assertEquals("Day 1: Intro",post.getContent());
+    }
+    @After
+    public void tearDown(){
+        Post.clearAllPost();
+    }
+    @Test
+    public void AllPostsAreCorrectlyReturned_true(){
+        Post post = new Post("Day 1: Intro");
+        Post otherPost = new Post("How to pair successfully");
+        assertEquals(2,Post.getAll().size());
+    }
+    @Test
+    public void AllPostsContainsAllPosts_true() {
+        Post post = new Post("Day 1: Intro");
+        Post otherPost = new Post ("How to pair successfully");
+        assertTrue(Post.getAll().contains(post));
+        assertTrue(Post.getAll().contains(otherPost));
     }
 }
