cdff61ab859a2ab9b5d2b60b611c0822ceba0de9
alexmwaura <alexmwaura43@gmail.com>
2020-11-03 20:56:39 +0300

    add getter methods

--- src/main/java/models/Post.java
+++ src/main/java/models/Post.java
@@ -1,13 +1,20 @@
 package models;
 
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 
 public class Post {
     private static ArrayList<Post> instances = new ArrayList<>();
     private String content;
+    private boolean published;
+    private LocalDateTime createdAt;
+    private int id;
     public Post(String content){
         this.content = content;
+        this.published = false;
+        this.createdAt = LocalDateTime.now();
         instances.add(this);
+        this.id = instances.size();
     }
     public String getContent(){
         return content;
@@ -17,5 +24,17 @@ public class Post {
     }
     public static void clearAllPost(){
         instances.clear();
+    }
+    public boolean getPublished(){
+        return published;
+    }
+    public LocalDateTime getCreatedAt(){
+        return createdAt;
+    }
+    public int getId(){
+        return id;
+    }
+    public static Post findById(int id) {
+        return instances.get(id-1);
     }
 }
